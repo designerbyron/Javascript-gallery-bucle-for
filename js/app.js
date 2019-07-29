814fdde2ad5b0c6749dd1a2b501fa0f696ca9b28
Byron Vanegas <designerbyron@gmail.com>
2019-07-29 11:40:13 -0600

    Mejoras al Diseño y se duplico la galeria

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let contenedorMiniaturas = document.getElementById("contenedorMiniaturas");
 
 for(let i =1; i<=4; i++ ){
     let nuevaImagen = document.createElement("img");
-    nuevaImagen.setAttribute("src", "img/pic"+i+".jpg");
+    nuevaImagen.setAttribute("src", "img/viajes-atlanta01/pic"+i+".jpg");
 
     contenedorMiniaturas.appendChild(nuevaImagen);
 
@@ -18,4 +18,27 @@ for(let i =1; i<=4; i++ ){
 
 function cambiarImagen(mySrc){
     imagenGrande.setAttribute("src", mySrc);
+}
+
+//Segunda Electronicos camaras
+
+let imagenGrandeElectronicos = document.getElementById("imagenGrandeElectronicos");
+
+let contenedorMiniaturasElectronicos = document.getElementById("contenedorMiniaturasElectronicos");
+
+for(let i =1; i<=4; i++ ){
+    let nuevaImagenElectronicos = document.createElement("img");
+    nuevaImagenElectronicos.setAttribute("src", "img/electronicos-camaras01/pic"+i+".jpg");
+
+    contenedorMiniaturasElectronicos.appendChild(nuevaImagenElectronicos);
+
+    nuevaImagenElectronicos.addEventListener("click", function(e){
+        let mySrcElectronicos = e.target.getAttribute("src");
+        cambiarImagenElectronicos(mySrcElectronicos);
+    })    
+
+}
+
+function cambiarImagenElectronicos(mySrcElectronicos){
+    imagenGrandeElectronicos.setAttribute("src", mySrcElectronicos);
 }
